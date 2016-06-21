7ce34225040c4009c530b28a2d04eeb9d2499722
dodekeract <florianwendgithub@gmail.com>
2016-06-21 17:32:09 +0200

    basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# impequid-service-provider
-Implements an impequid service provider, which stores information about other service providers.
+# Impequid Service Provider
+
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](http://opensource.org/licenses/MIT)
+[![Code Climate](https://codeclimate.com/github/dodekeract/impequid-service-provider/badges/gpa.svg)](https://codeclimate.com/github/dodekeract/impequid-service-provider)
+[![NPM Downloads](https://img.shields.io/npm/dm/impequid-service-provider.svg)](https://npmjs.com/package/impequid-service-provider)
+[![NPM Dependencies](https://david-dm.org/dodekeract/impequid-service-provider.svg)](https://npmjs.com/package/impequid-service-provider)
+[![Gitter Chatroom](https://badges.gitter.im/dodekeract/impequid.svg)](https://gitter.im/dodekeract/impequid)
+
+Implements an Impequid service provider, which stores information about Impequid Apps.
+
+## Intended Purpose
+
+Assuming you want to setup your own Impequid server, you most likely don't want to manage all information about Apps yourself. So you can just use any Impequid Service Provider, and let it manage the App information.
+
+The default Impequid Service provider is available at `https://services.impequid.com`.
+
+## Installation
+
+- Install [Node.js](https://nodejs.org)
+- Install [MongoDB](https://mongodb.org)
+- `git clone https://github.com/dodekeract/impequid-service-provider`
+- `cd impequid-service-provider`
+- `npm install`
+- `npm run build`
+
+It is highly recommended to run Impequid Service Provider behind a reverse proxy, like Nginx.
+
+## Usage
+
+- Starting: `npm run start`
+- Re-building (after changes): `npm run build`
+
+## Configuration
+
+Currently, you need to modify `config.json` in the `impequid-service-provider` directory:
+
+````json
+{
+	"listen": {
+		"address": "127.0.0.1",
+		"port": 50001
+	},
+	"mongo": {
+		"url": "mongodb://127.0.0.1/impequid-service-provider"
+	},
+	"server": {
+		"name": "Server Name",
+		"url": "server.url.com"
+	}
+}
+````
+
+## Exposed API
+
+- `GET /api/v1/basic/:appName` [example](https://services.impequid.com/api/v1/basic/dns.smartfl.at)
+	- returns the app information for app `:appName`
+
+- `GET /api/v1/multi/:appName,:appName,:appName,:appName` [example](https://services.impequid.com/api/v1/multi/dns.smartfl.at)
+	- returns the information for all requested apps
+
+- `GET /api/v1/all` [example](https://services.impequid.com/api/v1/all)
+	- returns the information about all apps
+	- useful mostly to initialize a local cache/mirror
---
+++ source/server/index.js
@@ -0,0 +1,150 @@
+// import external
+
+import 'babel-polyfill'; // generator functions
+import koa from 'koa';
+import koaRouter from 'koa-router';
+import http from 'http';
+import {default as mongoose, Schema} from 'mongoose';
+
+// config
+
+let customConfig;
+
+try {
+	customConfig = require('../../config.json');
+} catch (error) {
+	customConfig = {};
+}
+
+const config = Object.assign({
+	listen: {
+		address: '127.0.0.1',
+		port: 50001
+	},
+	mongo: {
+		url: 'mongodb://127.0.0.1/impequid-service-provider',
+		debug: true
+	},
+	server: {
+		name: 'Server Name',
+		url: 'server.url'
+	}
+}, customConfig);
+
+// mongoose
+
+mongoose.set('debug', config.mongo.debug);
+mongoose.connect(config.mongo.url, error => {
+	if (!error) {
+		console.info('Connected to MongoDB');
+	} else {
+		console.error(`could not connect to mongo: ${error}`);
+		process.exit(1);
+	}
+});
+
+const schema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	domain: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	url: {
+		type: String,
+		required: true
+	},
+	returnUrl: {
+		type: String,
+		required: true
+	},
+	author: {
+		pseudonym: {
+			type: String,
+			required: true
+		},
+		url: {
+			type: String,
+			required: true
+		},
+		name: {
+			type: String
+		}
+	}
+});
+
+const Model = mongoose.model('service', schema);
+
+// load service list into memory
+
+const service = new Map();
+
+Model.find({}, (error, entries) => {
+	entries.forEach(({name, domain, url, returnUrl, author}) => {
+		service.set(domain, {
+			name,
+			url,
+			returnUrl,
+			author
+		});
+	});
+});
+
+// routes
+
+const apiRouter = koaRouter();
+
+apiRouter.get('/basic/:domain', function * () {
+	const {domain} = this.params;
+	const result = service.get(domain);
+	if (result) {
+		this.body = result;
+	} else {
+		this.body = {
+			error: 'Not found'
+		};
+		this.status = 404;
+	}
+});
+
+apiRouter.get('/multi/:domainList', function * () {
+	const domainList = this.params.domainList.split(',');
+	const result = {};
+	domainList.forEach(domain => {
+		result[domain] = service.get(domain) || false;
+	});
+	this.body = result;
+});
+
+apiRouter.get('/all', function * () {
+	const result = {};
+
+	for (let [key, value] of service.entries()) {
+		result[key] = value;
+	}
+
+	this.body = result;
+});
+
+// attach routes
+
+const router = koaRouter();
+
+router.use('/api/v1', apiRouter.routes(), apiRouter.allowedMethods());
+
+// configure app
+
+const app = koa();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// start server
+
+const server = http.Server(app.callback());
+server.listen(config.listen.port, config.listen.address);
+server.on('listening', () => {
+	console.log(`listening on http://${config.listen.address}:${config.listen.port}`);
+});
